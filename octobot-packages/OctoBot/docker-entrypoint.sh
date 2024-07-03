440ca757d6e73372ff6ee03df49c1e164f58de23
Goša Lukyanau <dissfall@Gosas-Air.local>
2024-07-03 15:23:13 +0300

    update docker lauth script

--- octobot-packages/OctoBot/docker-entrypoint.sh
+++ octobot-packages/OctoBot/docker-entrypoint.sh
@@ -1,3 +1,3 @@
-/octobot/Octane tentacles -p /octobot/output/any_platform.zip -d /octobot-packages/reference_tentacles
-/octobot/Octane tentacles --install --all --location /octobot/output/any_platform.zip
-/octobot/Octane
+Octane tentacles -p /octobot/output/any_platform.zip -d /octobot-packages/reference_tentacles
+Octane tentacles --install --all --location /octobot/output/any_platform.zip
+Octane
